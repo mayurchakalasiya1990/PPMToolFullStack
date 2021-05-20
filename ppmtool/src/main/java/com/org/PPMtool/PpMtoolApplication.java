27d6046f7ba3e11be8dbd1114d3a8ccac89abe60
mayur chakalasiya <mayur.chakalasiya1990@gmail.com>
2021-05-20 09:35:07 +0530

    first commit

---
+++ ppmtool/src/main/java/com/org/PPMtool/PpMtoolApplication.java
@@ -0,0 +1,13 @@
+package com.org.PPMtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpMtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpMtoolApplication.class, args);
+	}
+
+}
---
+++ ppmtool/src/test/java/com/org/PPMtool/PpMtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.PPMtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpMtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
